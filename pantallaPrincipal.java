ce89c4a8a6008193ef2145575dcc3de4e2272b1b
GilmarGonzales21 <123492461+GilmarGonzales21@users.noreply.github.com>
2024-06-08 10:42:41 -0500

    Update pantallaPrincipal.java

--- pantallaPrincipal.java
+++ pantallaPrincipal.java
@@ -1,3 +1,14 @@
+import.javax.swing.*;
+import java.awt.event.*;
 public class pantallaPrincipal extends JFrame implements ActionListener{
-  
+private JLabel label1, label2;
+private JButton boton1;
+public pantallaPrincipal(){
+setLayout(null);
+label1 = new JLabel("Hogar");
+label1.setBounds();
+label1.setFont();
+label1.setForeground();
+add(label1);
+}
 }
